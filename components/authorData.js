b2f9f6676fb4ac39a1b23fb2bdd1e23d81c0a860
mattshardman <mattshardman@gmail.com>
2019-02-22 15:26:08 +0000

    remove unnessary author data file

--- components/authorData.js
+++
@@ -1,6 +0,0 @@
-export const authorData = {
-  title: 'Nick Smith',
-  subTitle: 'CFO Exonar',
-  intro: "Hi I'm Nick and I write about businesses and what not. Here are some articles I went and wrote, oh boy I do hope you like them", //eslint-disable-line
-  avatarImage: 'https://res.cloudinary.com/dgdniqfi9/image/upload/v1550655836/nick-blog/nick-smith.png',
-};
